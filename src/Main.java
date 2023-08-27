b32f7c7b841959320635fa2a976260dc8a567ff6
yali <yusufali0147@hotmail.com>
2023-08-28 01:44:30 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n,total=0;
+
+        Scanner imp =new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz : ");
+            n=imp.nextInt();
+            if (n%4==0){
+                total+=n;
+            }
+        }while (n%2==0);
+        System.out.println("Toplam : "+total);
     }
 }
